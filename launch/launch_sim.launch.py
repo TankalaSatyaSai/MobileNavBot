61a7c8fb1c26d75410a1aef5ef4b326ccdd2e619
Tankala Satya Sai <satyasai2004.edu@gmail.com>
2024-12-31 18:07:30 +0530

    added simulation files

---
+++ launch/launch_sim.launch.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from ament_index_python.packages import get_package_share_directory
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+import os
+
+def generate_launch_description():
+
+    pkg_name="mobile_robot_ros"
+
+    rsp= IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(pkg_name), 'launch', 'rsp.launch.py'
+        )]), launch_arguments={"use_sim_time": "true"}.items()
+    )
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py'
+        )])
+    )
+
+    spawn_entity = Node(
+        package="gazebo_ros",
+        executable="spawn_entity.py",
+        arguments=['-topic', 'robot_description', '-entity', 'my_robot'],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        rsp, 
+        gazebo, 
+        spawn_entity
+    ])

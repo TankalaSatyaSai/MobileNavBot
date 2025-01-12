e61baa3feb20457a3caf331eb7139aeabb4497e8
Tankala Satya Sai <satyasai2004.edu@gmail.com>
2025-01-12 19:26:29 +0530

    added new gazebo

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -2,16 +2,16 @@
 
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from ament_index_python.packages import get_package_share_directory
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 import os
 
 def generate_launch_description():
 
     pkg_name="mobile_robot_ros"
 
-    gazebo_params_file = os.path.join(get_package_share_directory(pkg_name), 'config', 'gazebo_params.yaml')
 
     rsp= IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
@@ -33,16 +33,32 @@ def generate_launch_description():
         remappings=[('cmd_vel_out', 'diff_cont/cmd_vel/unstamped')]
     )
 
+    default_world=os.path.join(
+        get_package_share_directory(pkg_name),
+        'worlds',
+        'empty.world'
+    ) 
+
+    world = LaunchConfiguration('world')
+
+    world_arg=DeclareLaunchArgument(
+        'world',
+        default_value=default_world,
+        description="world to load"
+    ) 
+
     gazebo = IncludeLaunchDescription(  
         PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py'
-        )]), launch_arguments={"extra_gazebo_args": "--ros-args --params-file" + gazebo_params_file}.items()
+            get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py'
+        )]), launch_arguments={'gz_args': ['-r -v4 ', world], 'on_exit_shutdown': 'true'}.items()
     )
 
     spawn_entity = Node(
-        package="gazebo_ros",
-        executable="spawn_entity.py",
-        arguments=['-topic', 'robot_description', '-entity', 'my_robot'],
+        package="ros_gz_sim",
+        executable="create",
+        arguments=['-topic', 'robot_description', 
+                   '-name', 'my_robot', 
+                   '-z', '0.1'],
         output='screen'
     )
 
@@ -58,12 +74,34 @@ def generate_launch_description():
         arguments=["joint_broad"]
     )
 
+    gz_bridge_params = os.path.join(get_package_share_directory(pkg_name), 'config', 'gz_bridge.yaml')
+
+    ros_gz_bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=[
+            '--ros-args',
+            '-p',
+            f'config_file:={gz_bridge_params}',
+        ]
+    )
+
+    ros_gz_image_bridge=Node(
+        package='ros_gz_image',
+        executable='image_bridge',
+        arguments=["/camera/image_raw"]
+    )
+
+
     return LaunchDescription([
         rsp, 
         # teleop_kbd,
         twist_mux,
+        world_arg,
         gazebo, 
         spawn_entity,
         diff_drive_spawner,
-        joint_broad_spawner
+        joint_broad_spawner,
+        ros_gz_bridge,
+        ros_gz_image_bridge
     ])
